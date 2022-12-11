36858944975faf6f563d2e6562240785277e38c8
gegeeem <gegeeem@gmail.com>
2022-12-11 18:43:07 +0100

    generic type for reading object props

--- typedObjArr/ObjectWithTypesProps.js
+++ typedObjArr/ObjectWithTypesProps.js
@@ -1,8 +1,13 @@
 var alice = {
     name: "Alice",
-    age: 25
+    age: 25,
+    role: "admin"
 };
 var blake = "Blejk";
 console.log(typeof blake);
 var propKeys = "age";
-console.log("propKeys", propKeys);
+console.log("propKeys->", propKeys);
+var valueOfUser = 95;
+console.log("valueOfUser->", valueOfUser);
+var student = 'standard';
+console.log('student->', student);
--- typedObjArr/ObjectWithTypesProps.ts
+++ typedObjArr/ObjectWithTypesProps.ts
@@ -1,10 +1,12 @@
 type User = {
   name: string;
   age: number;
+  role: "admin" | "standard";
 };
 const alice: User = {
   name: "Alice",
   age: 25,
+  role: "admin",
 };
 // type age = User.age => syntax error!
 type name = User["name"];
@@ -14,3 +16,15 @@ console.log(typeof blake);
 type Keys = keyof User;
 const propKeys: Keys = "age";
 console.log("propKeys->", propKeys);
+
+type UserValues = User[keyof User];
+const valueOfUser: UserValues = 95;
+
+console.log("valueOfUser->", valueOfUser);
+
+//common ppatern for generic Object
+type ValueOf<Obj> = Obj[keyof Obj];
+type UserValuesFromGeneric = ValueOf<User>;
+
+const student: UserValuesFromGeneric = 'standard';
+console.log('student->', student);
